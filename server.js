5400dcce5c4f4ac01305b3ab0bb76a6ba465632b
mzwae <mrmonjaie@gmail.com>
2017-11-21 08:04:38 +0000

    Added Angular Routes

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var bodyparser = require('body-parser');
 var app = express();
-app.use(logger('short'));
+app.use(logger('dev'));
 
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'angularjs')));
-//app.set('views', path.join(__dirname, 'angularjs', 'app.html'));
-//console.log("__dirname:", path.join(__dirname, 'app.html'));
+app.set('views', path.join(__dirname, 'angularjs'));
+
+
 app.get('/', function(req, res){
-  res.redirect('/app.html');
+  res.sendFile(path.join(__dirname, 'angularjs', 'app.html'));
+  
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Server listening on port 3000...");
 });
-
-
